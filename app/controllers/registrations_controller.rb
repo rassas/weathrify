d2730250a67c5aca1521e5d422d17f5a5a234778
Rassas Achref <arassas.externe@effilab.com>
2024-12-15 00:56:06 +0100

    Registration

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,23 @@
+class RegistrationsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(registration_params)
+
+    if @user.save
+      flash[:notice] = "Signed up successfully"
+      redirect_to root_path
+    else
+      flash[:alert] = "User not created"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def registration_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+end
