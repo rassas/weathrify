674fc77166bf54b9566de94d8fd2d9ca8ae32818
achref rassas <rassasachref@gmail.com>
2024-12-18 20:24:15 +0100

    rename weather service

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -2,7 +2,7 @@ class WeathersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @weather_data = Services::Weather::WeatherService.new(lat: lat_param, lng: lng_param).call
+    @weather_data = Services::Weather::CurrentWeatherWithForecastService.new(lat: lat_param, lng: lng_param).call
   rescue Weatherbit::WeatherbitApiError
     render :index, flash: { error: "Something went wrong! You may reached the limit of 50 calls per day!" }
   end
