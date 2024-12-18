32e24080be55beb9716be4cfe4c839da084497a3
achref rassas <rassasachref@gmail.com>
2024-12-18 21:53:46 +0100

    lint

--- app/controllers/api/v1/temperatures_controller.rb
+++ app/controllers/api/v1/temperatures_controller.rb
@@ -5,20 +5,20 @@ module Api
 
       # Expected format: /api/v1/temperatures?cities=London,Paris,Tokyo
       def index
-        return render json: { error: 'No cities provided' }, status: :bad_request if city_list.blank?
+        return render json: { error: "No cities provided" }, status: :bad_request if city_list.blank?
 
         average_temp = Services::Weather::TemperatureService.new(city_list).call
         render json: { average_temperature: average_temp.round(2), cities: city_list }
       rescue Services::Weather::TemperatureService::FetchTemperatureError => e
         render json: { error: e.message }
       rescue Weatherbit::WeatherbitApiError
-        render json: { error: "Something went wrong! You may reached the limit of 50 calls per day!"}
+        render json: { error: "Something went wrong! You may reached the limit of 50 calls per day!" }
       end
 
       private
 
       def city_list
-        @city_list ||= params[:cities]&.split(',')&.map(&:strip)
+        @city_list ||= params[:cities]&.split(",")&.map(&:strip)
       end
     end
   end
